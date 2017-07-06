47091165fe109be8d5caa558de9cd6c63b666105
Cole Maclean <hi@cole.io>
2017-07-05 23:26:33 -0700

    Don't append to fish_user_paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,11 +6,6 @@
 set PATH /usr/local/bin /usr/local/sbin /usr/sbin /usr/bin /sbin /bin
 set MANPATH /usr/local/share/man /usr/share/man
 
-# Add the scripts folder
-if test -d $HOME/Documents/Scripts
-    set -U fish_user_paths $fish_user_paths $HOME/Documents/Scripts
-end
-
 # Set up some variables for 'screen'
 if test -d $HOME/.screen
     set -x SCREENDIR $HOME/.screen
@@ -38,21 +33,13 @@ if which pyenv > /dev/null; and status --is-interactive
     . (pyenv init -|psub)
 end
 
+if which rbenv > /dev/null; and status --is-interactive
+    source (rbenv init -|psub)
+end
+
 # Shell autocomplete
 if test -e $HOME/.pythonrc.py
     set -x PYTHONSTARTUP $HOME/.pythonrc.py
-end
-
-# Add NPM global packages bin folder
-if which npm > /dev/null
-set -U fish_user_paths $fish_user_paths (npm -g bin)
-end
-
-# rbenv
-if which rbenv > /dev/null
-    set -U fish_user_paths $HOME/.rbenv/bin $fish_user_paths
-    set -U fish_user_paths $HOME/.rbenv/shims $fish_user_paths
-    rbenv rehash >/dev/null ^&1
 end
 
 # Source any API access keys
